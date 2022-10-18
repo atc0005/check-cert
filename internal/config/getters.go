bbca196e83a4bbc42e691097056fadc7d1d5ccc7
Adam Chalkley <atc0005@users.noreply.github.com>
2022-10-18 06:26:53 -0500

    Replace duplicate valid log levels entry
    Restore ability to use `disabled` as a valid log level per
    CLI flag.
    
    refs GH-427

--- internal/config/getters.go
+++ internal/config/getters.go
@@ -177,11 +177,11 @@ func supportedValidationCheckResultKeywords() []string {
 	}
 }
 
-// supportedValidationCheckResultKeywords returns a list of valid log levels
-// supported by tools in this project.
+// supportedLogLevels returns a list of valid log levels supported by tools in
+// this project.
 func supportedLogLevels() []string {
 	return []string{
-		LogLevelDebug,
+		LogLevelDisabled,
 		LogLevelPanic,
 		LogLevelFatal,
 		LogLevelError,
