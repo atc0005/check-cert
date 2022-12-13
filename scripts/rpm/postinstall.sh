aaf96449662c7427711e809d2ce6c763a1640a28
Adam Chalkley <atc0005@users.noreply.github.com>
2022-12-13 04:57:34 -0600

    Generate DEB & RPM packages using nFPM
    Use new 'packages' Makefile recipe to build DEB and RPM packages
    using the nFPM tool from the GoReleaser project.
    
    refs GH-439
    
    See also:
    
    - https://nfpm.goreleaser.com/
    - https://github.com/goreleaser/nfpm
    
    Thanks to the nFPM project for this awesome tool!

---
+++ scripts/rpm/postinstall.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Copyright 2023 Adam Chalkley
+#
+# https://github.com/atc0005/check-cert
+#
+# Licensed under the MIT License. See LICENSE file in the project root for
+# full license information.
+
+project_org="atc0005"
+project_shortname="check-cert"
+
+project_fq_name="${project_org}/${project_shortname}"
+project_url_base="https://github.com/${project_org}"
+project_repo="${project_url_base}/${project_shortname}"
+project_releases="${project_repo}/releases"
+project_issues="${project_repo}/issues"
+project_discussions="${project_repo}/discussions"
+
+plugin_name="check_cert"
+plugin_path="/usr/lib64/nagios/plugins"
+
+# Set required SELinux context to allow plugin use when SELinux is enabled.
+if [ -f "${plugin_path}/${plugin_name}" ]; then
+    chcon \
+        --verbose \
+        -t nagios_unconfined_plugin_exec_t \
+        -u system_u \
+        -r object_r \
+        /usr/lib64/nagios/plugins/check_cert
+
+    if [ $? -eq 0 ]; then
+        echo "Successfully applied SELinux contexts on ${plugin_path}/${plugin_name}"
+    else
+        echo "Failed to set SELinux contexts on ${plugin_path}/${plugin_name}"
+    fi
+
+else
+    echo "${plugin_path}/${plugin_name} could not be found!"
+fi
+
+echo
+echo "Thank you for installing packages provided by the ${project_fq_name} project!"
+echo
+echo "Project resources:"
+echo
+echo "- Obtain latest release: ${project_releases}"
+echo "- View/Ask questions: ${project_discussions}"
+echo "- View/Open issues: ${project_issues}"
+echo
+
+
+# Copying contexts from existing plugin:
+#
+# [root@atc0005-rhel8-test ~]# ls -laZ /usr/lib64/nagios/plugins/check_by_ssh
+# -rwxr-xr-x. root root system_u:object_r:nagios_unconfined_plugin_exec_t:s0 /usr/lib64/nagios/plugins/check_by_ssh
+# [root@atc0005-rhel8-test ~]# sudo chcon --verbose --reference /usr/lib64/nagios/plugins/check_by_ssh /usr/lib64/nagios/plugins/check_cert
+
+# Explicitly applying contexts:
+#
+# [root@atc0005-rhel8-test ~]# sudo chcon --verbose -t nagios_unconfined_plugin_exec_t -u system_u -r object_r /usr/lib64/nagios/plugins/check_cert
+# changing security context of '/usr/lib64/nagios/plugins/check_cert'
+#
+# [root@atc0005-rhel8-test ~]# ls -laZ /usr/lib64/nagios/plugins/check_cert
+# -rwxr-xr-x. 1 root root system_u:object_r:nagios_unconfined_plugin_exec_t:s0 6035136 Dec 12 11:14 /usr/lib64/nagios/plugins/check_cert
